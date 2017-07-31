3651f05ddf0a0d1d3dc8207585a4a28fd860d3c3
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-01 00:10:56 +0100

    headers request options

--- src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Passenger } from './models/passenger.interface';
 
 import { Observable } from 'rxjs/Observable';
@@ -20,9 +20,16 @@ export class PassengerDashboardService {
           .map( (response: Response) => response.json());
     }
 
+
     updatePassenger(passenger: Passenger): Observable<Passenger> {
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        let options = new RequestOptions({
+            headers: headers
+        });
         return this.http
-          .put(`${PASSENGER_API}/${passenger.id}`, passenger)
+          .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
           .map( (response: Response) => response.json());
     }
 
