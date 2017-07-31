c6c75bf29c56b422c8a2c84d0f85f36fa207b188
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-01 00:01:25 +0100

    http put delete

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -38,22 +38,32 @@ export class PassengerDashboardComponent implements OnInit {
   }
   
   handleRemove(event: Passenger){
-    this.passengers = this.passengers.filter((passenger: Passenger) => {
-        return passenger.id !== event.id;
+
+    this.passengerService
+      .removePassenger(event)
+      .subscribe((data: Passenger)=> {
+          this.passengers = this.passengers.filter((passenger: Passenger) => {
+                return passenger.id !== event.id;
+      })
     });  
   }
 
   handleEdit(event: Passenger) {
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      // detects if we are in the current passenger that has been 
-      // fired from the handleEdit event; takes the original 
-      // passenger object and merges the latest changes of the 
-      // event in.
-      if (passenger.id === event.id){
-          // we use an immutable operation 
-          passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
-    });
+
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+              // detects if we are in the current passenger that has been 
+              // fired from the handleEdit event; takes the original 
+              // passenger object and merges the latest changes of the 
+              // event in.
+              if (passenger.id === event.id){
+                  // we use an immutable operation 
+                  passenger = Object.assign({}, passenger, event);
+              }
+              return passenger;
+            });
+      });
   }
 }
