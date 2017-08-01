0d9757e724a7dce516e9afcf23c7aea89fb66358
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-01 22:26:53 +0100

    create input with type radio

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -21,6 +21,34 @@ import { Passenger} from '../../models/passenger.interface';
                     name="id"
                     [ngModel]="detail?.id"> <!-- ? safe navigation operator -->
             </div>
+            <div>
+                <label>
+                    <input type="radio"
+                        [value]="true"
+                        name="checkedIn"
+                        [ngModel]="detail?.checkedIn"
+                        (ngModelChange)="toggleCheckIn($event)">
+                    Yes
+                </label>
+                <label>
+                    <input type="radio"
+                        [value]="false"
+                        name="checkedIn"
+                        [ngModel]="detail?.checkedIn"
+                        (ngModelChange)="toggleCheckIn($event)">
+                    No
+                </label>
+
+            </div>
+
+            <div *ngIf="form.value.checkedIn">
+                Check in date:
+                <input 
+                    type="number"
+                    name="checkInDate"
+                    [ngModel]="detail?.checkInDate">
+
+            </div>
             {{ form.value | json }}
         </form>
     `
@@ -28,5 +56,10 @@ import { Passenger} from '../../models/passenger.interface';
 export class PassengerFormComponent {
     @Input()
     detail: Passenger;
-
+    
+    toggleCheckIn(checkedIn: boolean){
+        if(checkedIn) {
+            this.detail.checkInDate = Date.now();
+        }
+    }
 }
