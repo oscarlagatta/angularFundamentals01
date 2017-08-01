3749f615d9e659ba83e878ee3194b718312656c1
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-01 23:19:51 +0100

    create static array in a select element and options

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Passenger} from '../../models/passenger.interface';
-
+import { Baggage } from '../../models/baggage.interface';
 @Component({
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
@@ -39,14 +39,57 @@ import { Passenger} from '../../models/passenger.interface';
                     [ngModel]="detail?.checkInDate">
 
             </div>
+            <div>
+                Luggage:
+                <select
+                    name="baggage"
+                    [ngModel]="detail?.baggage"
+                    >
+                    <option *ngFor="let item of baggage" 
+                            [value]="item.key"
+                            [selected]="item.key === detail?.baggage">
+                             {{ item.value }}   
+                    </option>
+                </select>
+
+                <!-- in case you know the api you can use the ngValue -->
+                <select
+                    name="baggage"
+                    [ngModel]="detail?.baggage"
+                    >
+                    <option *ngFor="let item of baggage" 
+                            [ngValue]="item.key"
+                            [selected]="item.key === detail?.baggage">
+                             {{ item.value }}   
+                    </option>
+                </select>
+            </div>
             {{ form.value | json }}
         </form>
     `
 })
 export class PassengerFormComponent {
+    
     @Input()
     detail: Passenger;
     
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No baggage'
+    },
+    {
+        key: 'hand-only',
+        value: 'Hand baggage'
+    },
+    {
+        key: 'hold-only',
+        value: 'Hold baggage'
+    },
+    {
+        key: 'hand-hold',
+        value: 'Hand and Hold baggage'
+    }];
+
     toggleCheckIn(checkedIn: boolean){
         if(checkedIn) {
             this.detail.checkInDate = Date.now();
