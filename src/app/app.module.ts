700e7431cb08bf1947de5ce0e61049cada8e1f6c
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-02 22:53:50 +0100

    redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'passengers', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
@@ -23,7 +23,7 @@ const routes: Routes = [
     // angular modules
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     // custom modules
     PassengerDashboardModule
   ],
